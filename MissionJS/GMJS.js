021b11192747f3c1fa82023ee127ee69f4eecbef
Nisha <nishamohnish@gmail.com>
2022-02-14 09:59:48 +1300

    first JavaScript

---
+++ MissionJS/GMJS.js
@@ -0,0 +1,28 @@
+//Event + Function
+function mOver(obj){
+    obj.innerHTML = "On Specials!!!"
+}
+   
+function mOut ( obj){
+    obj.innerHTML = "Get $250 off!!!";
+}
+
+//Get element by id
+
+function para(){
+    document.getElementById("para").style.fontSize ="X-large";
+};
+
+
+//Use or Var , Let or Const
+function myFunction() {
+    let text;
+    let person = prompt("Please enter your name:", "First & Last");
+    if (person == null || person == "") {
+      text = "Hello! how can we help";
+    } else {
+      text = "Welcome Back"+ " " + person + " " +"! How are you today?";
+    }
+    document.getElementById("demo").innerHTML = text;
+  }
+    
