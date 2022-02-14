a1f603a87d119ccad857de44c48b181208b9df6b
Nisha <nishamohnish@gmail.com>
2022-02-14 14:31:53 +1300

    .text method used

--- JQuery/JQ.js
+++ JQuery/JQ.js
@@ -1,4 +1,9 @@
 
 // document.getElementById('mainContent').innerText = 'Hello there';
+// 1
+// $('#mainContent').text("Hey there");
 
-$('#mainContent').text("Hey there");
+//2 // .text() method without any parameters gives you the existing element value
+
+const mainParaJ = $('#mainContent');
+console.log('mainParaJ.text() is ', mainParaJ.text());mainParaJ.text(mainParaJ.text() + ' Nisha');
