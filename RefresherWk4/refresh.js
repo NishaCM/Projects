d173fef301fa710b961e6259de1cb5e821bd63c4
Nisha <nishamohnish@gmail.com>
2022-02-16 10:58:17 +1300

    examples

---
+++ RefresherWk4/refresh.js
@@ -0,0 +1,29 @@
+//ternary operator 1
+// const word = "hippopotamus";
+// word[0] === 'h'? console.log('the word might be hippopotamus') : console.log('the word is definitely not hippopotamus');
+
+//ex 2
+
+// function checkIfEven(num) {  if(num % 2 === 0)   {    return true;  }  else {    return false  }} console.log("13 is even? " ,checkIfEven(13));console.log("24 is even? " ,checkIfEven(24));
+// function checkIfEven(num) {  return (num % 2 === 0) ? true : false;} 
+
+//Loops for ( let i = 0; i<5; i++){}
+//ex 3
+// for(let i = 0; i<5; i++){
+    
+//     console.log ('Nisha');
+// }
+//Question - run a loop 10 - 1
+// for(let i = 10; i>0; i--){
+    //     console.log (i);}
+
+
+//Function Arrow 
+//EX4
+function twoPara(A ,B){
+    console.log(A,B);
+};
+
+twoPara();
+
+//EX5
