b17ae0298ca65d201d69bc3c6c8080b4d76e5d78
Nisha <nishamohnish@gmail.com>
2022-02-22 13:32:30 +1300

    learning react

---
+++ React/hello-reactcd/src/App.js
@@ -0,0 +1,18 @@
+const message1 = 'Hey you';
+const message2 = "What's going on";
+
+
+function App() {
+  return (
+    <div>
+      <h1>{message1}</h1>
+      <p>{message2 + '??'}</p>
+    </div>
+  )
+    
+  }
+
+export default App;
+
+
+
